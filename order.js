f19517eb5d7772695c0dcc308cd23db8379f4a3e
aviacharya01 <aviacharyasr@gmail.com>
2024-04-12 00:53:25 +0530

    FilesAdded

---
+++ order.js
@@ -0,0 +1,32 @@
+const Orders=[
+    { 
+        productName: "iphone 15",
+        productNumber: "678987",
+        paymentStatus: 'Due',
+        shipping: 'Pending',
+    },
+    {
+        productName: "iphone 15",
+        productNumber: "678987",
+        paymentStatus: 'Due',
+        shipping: 'Pending',
+    },
+    {
+        productName: "iphone 15",
+        productNumber: "678987",
+        paymentStatus: 'Due',
+        shipping: 'Pending',
+    },
+    {
+        productName: "iphone 15",
+        productNumber: "678987",
+        paymentStatus: 'Due',
+        shipping: 'Pending',
+    },
+    {
+        productName: "iphone 15",
+        productNumber: "678987",
+        paymentStatus: 'Due',
+        shipping: 'Pending',
+    }
+]
